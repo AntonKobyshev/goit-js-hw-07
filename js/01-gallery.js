4fe2154f214cff338ea28814b700c7e32be5a010
AntonKobyshev <antonkobyshev@gmail.com>
2023-02-23 15:25:05 +0200

    render

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+const markup = createGalleryMarkup(galleryItems);
+// galleryRef.innerHTML = markup;
+galleryRef.insertAdjacentHTML('beforeend', markup);
+
+function createGalleryMarkup(items) {
+    // const markup =
+    return    items.map(({ preview, original, description }) => {
+    return `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`;
+  })
+.join('');
+}
+
+// console.log(galleryItems);
