88bd60323f9d8f3a443b0004746ee4e3963649a1
AntonKobyshev <antonkobyshev@gmail.com>
2023-02-23 20:33:39 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,12 +4,14 @@ import { galleryItems } from "./gallery-items.js";
 const galleryRef = document.querySelector(".gallery");
 const markup = createGalleryMarkup(galleryItems);
 // galleryRef.innerHTML = markup;
-galleryRef.insertAdjacentHTML('beforeend', markup);
+galleryRef.insertAdjacentHTML("beforeend", markup);
+galleryRef.addEventListener("click", onImageClick);
 
 function createGalleryMarkup(items) {
-    // const markup =
-    return    items.map(({ preview, original, description }) => {
-    return `
+  // const markup =
+  return items
+    .map(({ preview, original, description }) => {
+      return `
 <div class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
@@ -21,8 +23,44 @@ function createGalleryMarkup(items) {
   </a>
 </div>
 `;
-  })
-.join('');
+    })
+    .join("");
+}
+
+const instance = basicLightbox.create(`
+    <img src="" width="1280" height="auto">`,
+
+ {
+    onShow: (instance) => {
+      window.addEventListener('keydown', onEscKeyPress);
+    },
+    onClose: (instance) => {
+      window.removeEventListener('keydown', onEscKeyPress);
+    },
+ }
+
+ );
+
+
+function onImageClick(e) {
+  e.preventDefault();
+
+  // if (e.target.nodeName !== 'IMG') {
+  //     return;
+  // }
+
+  const datasetSource = e.target.dataset.source;
+  if (!datasetSource) return;
+  instance.element().querySelector("img").src = datasetSource;
+
+  instance.show();
+  console.log(e.target);
+}
+
+
+function onEscKeyPress(e) {
+  if (e.code !== 'Escape') return;
+  instance.close();
 }
 
 // console.log(galleryItems);
